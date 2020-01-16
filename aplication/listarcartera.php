0cdb55396714d2085d5ecedcac750b44bb328289
jorgeochoa1987 <jeob2046@gmail.com>
2020-01-15 23:08:10 -0500

    Mod Cartera

--- aplication/listarcartera.php
+++ aplication/listarcartera.php
@@ -29,42 +29,34 @@ include('header.php');
                           <th scope="col" class="border-0">Telefono</th>
                           <th scope="col" class="border-0">Correo</th>
                           <th scope="col" class="border-0">Valor</th>
+                          <th scope="col" class="border-0">Favorito</th>
+                          <th scope="col" class="border-0">Modificar</th>
+                          <th scope="col" class="border-0">Eliminar</th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                          <td>1</td>
-                          <td>Ali</td>
-                          <td>Kerry</td>
-                          <td>Russian Federation</td>
-                          <td>Gdańsk</td>
-                          <td>107-0339</td>
-                        </tr>
-                        <tr>
-                          <td>2</td>
-                          <td>Clark</td>
-                          <td>Angela</td>
-                          <td>Estonia</td>
-                          <td>Borghetto di Vara</td>
-                          <td>1-660-850-1647</td>
-                        </tr>
-                        <tr>
-                          <td>3</td>
-                          <td>Jerry</td>
-                          <td>Nathan</td>
-                          <td>Cyprus</td>
-                          <td>Braunau am Inn</td>
-                          <td>214-4225</td>
-                        </tr>
-                        <tr>
-                          <td>4</td>
-                          <td>Colt</td>
-                          <td>Angela</td>
-                          <td>Liberia</td>
-                          <td>Bad Hersfeld</td>
-                          <td>1-848-473-7416</td>
-                        </tr>
-                      </tbody>
+                        
+                         <?php 
+                          require('../conex/conexion.php');
+                          $query="SELECT * FROM cartera  where saldo > 0 ";
+                          $answer = $conexion -> query($query);
+                          while ($row=$answer->fetch_assoc()){
+                          ?>
+                          <tr>
+                              <td> <?php echo $row['id']; ?></td>
+                              <td> <?php echo $row['nombre']; ?></td>
+                              <td> <?php echo $row['Apellido']; ?></td>
+                              <td> <?php echo $row['telefono']; ?></td>
+                              <td> <?php echo $row['correo']; ?></td>
+                              <td> <?php echo $row['valorIni']; ?></td>
+                              <td> <?php echo $row['favorito']; ?></td>
+                              <td> <a href="modCartera.php?id=<?php echo $row ['id'];?>">Modificar </a></td>
+                              <td> <a href="#">Eliminar </a></td>
+                          </tr>
+                          <?php 
+                          }
+                          ?>
+                        </tbody>
                     </table>
                   </div>
                 </div>
@@ -83,46 +75,37 @@ include('header.php');
                       <thead class="thead-dark">
                         <tr>
                         <th scope="col" class="border-0">#</th>
-                          <th scope="col" class="border-0">Nombre</th>
+                           <th scope="col" class="border-0">Nombre</th>
                           <th scope="col" class="border-0">Apellido</th>
                           <th scope="col" class="border-0">Telefono</th>
                           <th scope="col" class="border-0">Correo</th>
                           <th scope="col" class="border-0">Valor</th>
+                          <th scope="col" class="border-0">Favorito</th>
+                          <th scope="col" class="border-0">Modificar</th>
+                          <th scope="col" class="border-0">Eliminar</th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                          <td>1</td>
-                          <td>Graham</td>
-                          <td>Brent</td>
-                          <td>Benin</td>
-                          <td>Ripabottoni</td>
-                          <td>1-512-760-9094</td>
-                        </tr>
-                        <tr>
-                          <td>2</td>
-                          <td>Clark</td>
-                          <td>Angela</td>
-                          <td>Estonia</td>
-                          <td>Borghetto di Vara</td>
-                          <td>1-660-850-1647</td>
-                        </tr>
-                        <tr>
-                          <td>3</td>
-                          <td>Wylie</td>
-                          <td>Joseph</td>
-                          <td>Korea, North</td>
-                          <td>Guelph</td>
-                          <td>325-4351</td>
-                        </tr>
-                        <tr>
-                          <td>4</td>
-                          <td>Garth</td>
-                          <td>Clementine</td>
-                          <td>Indonesia</td>
-                          <td>Narcao</td>
-                          <td>722-8264</td>
-                        </tr>
+                      <?php 
+                          require('../conex/conexion.php');
+                          $query="SELECT * FROM cartera where saldo =0";
+                          $answer = $conexion -> query($query);
+                          while ($row=$answer->fetch_assoc()){
+                          ?>
+                          <tr>
+                              <td> <?php echo $row['id']; ?></td>
+                              <td> <?php echo $row['nombre']; ?></td>
+                              <td> <?php echo $row['Apellido']; ?></td>
+                              <td> <?php echo $row['telefono']; ?></td>
+                              <td> <?php echo $row['correo']; ?></td>
+                              <td> <?php echo $row['valorIni']; ?></td>
+                              <td> <?php echo $row['favorito']; ?></td>
+                              <td> <a href="modCartera.php?id=<?php echo $row ['id'];?>">Modificar </a></td>
+                              <td> <a href="#">Eliminar </a></td>
+                          </tr>
+                          <?php 
+                          }
+                          ?>
                       </tbody>
                     </table>
                   </div>
